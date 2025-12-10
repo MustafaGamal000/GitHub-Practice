b2475149a3b72e630fb0e586b3c3921eaf0c16b5
hazemkhaled241 <hazemkhaled3166589@gmail.com>
2025-12-10 16:15:28 +0200

    Add new feature (Evening)

---
+++ src/main/java/com/example/github/service/EveningService.java
@@ -0,0 +1,11 @@
+package com.example.github.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EveningService {
+    public String sayGoodEvening() {
+        return "Good Evening";
+    }
+
+}
