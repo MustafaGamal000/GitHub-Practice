a1cb3bd7ef88918c0abeba08078edc9dd2939f09
MustafaGamal000 <mustafa.gamal000@gmail.com>
2025-12-10 15:25:14 +0200

    add good morning feature

---
+++ src/main/java/com/example/github/service/MorningService.java
@@ -0,0 +1,11 @@
+package com.example.github.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MorningService {
+    public String sayGoodMorning() {
+        return "Good Morning";
+    }
+
+}
