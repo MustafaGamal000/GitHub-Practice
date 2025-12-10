4ac4390d51f6a25e3ca33c19c44d7520e7dd9d2c
AbbasGhulam <abbasbanani94@gmail.com>
2025-12-10 17:23:42 +0300

    Added Afternoon feature

--- src/main/java/com/example/github/controller/GreetingController.java
+++ src/main/java/com/example/github/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.github.controller;
 
+import com.example.github.service.AfternoonService;
 import com.example.github.service.GreetingService;
 import com.example.github.service.MorningService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
     private final GreetingService greetingService;
     private final MorningService morningService;
+    private final AfternoonService afternoonService;
 
-    public GreetingController(GreetingService greetingService, MorningService morningService) {
+    public GreetingController(GreetingService greetingService, MorningService morningService, AfternoonService afternoonService) {
         this.greetingService = greetingService;
         this.morningService = morningService;
+        this.afternoonService = afternoonService;
     }
     @GetMapping("/hello")
     public String hello() {
@@ -22,5 +25,10 @@ public class GreetingController {
     @GetMapping("/morning")
     public String morning() {
         return morningService.sayGoodMorning();
+    }
+
+    @GetMapping("/afternoon")
+    public String afternoon() {
+        return afternoonService.sayGoodAfternoon();
     }
 }
---
+++ src/main/java/com/example/github/service/AfternoonService.java
@@ -0,0 +1,11 @@
+package com.example.github.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AfternoonService {
+
+    public String sayGoodAfternoon() {
+        return "Good Afternoon";
+    }
+}
